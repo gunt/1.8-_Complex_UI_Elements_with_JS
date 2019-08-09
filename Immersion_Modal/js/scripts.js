21c8a9903db5060645788de50fd63c3e11bcba2e
gunt <4261629+gunt@users.noreply.github.com>
2019-08-09 08:27:35 -0600

    modal creation

--- Immersion_Modal/js/scripts.js
+++ Immersion_Modal/js/scripts.js
@@ -1,10 +1,54 @@
-
-function showModal() {
-    var $modalContainer = document.querySelector('modal-container');
-    $modalContainer.classList.add('is-visible');
-}
-
-document.querySelector('#show-modal').addEventListener('click', () => {
-    showModal();
-});
-
+(function() {
+    var $modalContainer = document.querySelector('#modal-container');
+    
+    function showModal(title, text) {
+      // Clear all existing modal content
+      $modalContainer.innerHTML = '';
+      
+      var modal = document.createElement('div');
+      modal.classList.add('modal');
+      
+      // Add the new modal content
+      var closeButtonElement = document.createElement('button');
+      closeButtonElement.classList.add('modal-close');
+      closeButtonElement.innerText = 'Close';
+      closeButtonElement.addEventListener('click', hideModal);
+      
+      var titleElement = document.createElement('h1');
+      titleElement.innerText = title;
+      
+      var contentElement = document.createElement('p');
+      contentElement.innerText = text;
+      
+      modal.appendChild(closeButtonElement);
+      modal.appendChild(titleElement);
+      modal.appendChild(contentElement);
+      $modalContainer.appendChild(modal);
+      
+      $modalContainer.classList.add('is-visible');
+    }
+    
+    function hideModal() {
+      $modalContainer.classList.remove('is-visible');
+    }
+    
+    document.querySelector('#show-modal').addEventListener('click', () => {
+      showModal('Modal title', 'This is the modal content!');
+    });
+    
+    window.addEventListener('keydown', (e) => {
+      if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+        hideModal();  
+      }
+    });
+    
+    $modalContainer.addEventListener('click', (e) => {
+      // Since this is also triggered when clicking INSIDE the modal container,
+      // We only want to close if the user clicks directly on the overlay
+      var target = e.target;
+      if (target === $modalContainer) {
+        hideModal();
+      }
+    });
+    
+  })();
