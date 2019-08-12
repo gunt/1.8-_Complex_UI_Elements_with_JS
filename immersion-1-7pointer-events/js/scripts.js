3a0e0cda1b0205f1ddf592bd34d96a3c809d7d58
gunt <4261629+gunt@users.noreply.github.com>
2019-08-12 12:14:35 -0600

    coding pointer events

---
+++ immersion-1-7pointer-events/js/scripts.js
@@ -0,0 +1,20 @@
+(function() {
+    var $canvas = document.querySelector('#canvas');
+    
+    function handleStart(e) {
+      
+    }
+  
+    function handleEnd() {
+      
+    }
+  
+    function handleMove(e) {
+      
+    }
+  
+    $canvas.addEventListener("pointerdown", handleStart);
+    $canvas.addEventListener("pointerup", handleEnd);
+    $canvas.addEventListener("pointercancel", handleEnd);
+    $canvas.addEventListener("pointermove", handleMove);
+  })();
