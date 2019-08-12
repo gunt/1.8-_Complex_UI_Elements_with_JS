c491173992811ddc3f32d7b6a523a5a775263f17
gunt <4261629+gunt@users.noreply.github.com>
2019-08-12 12:28:28 -0600

    adding two variable - actual drawing

--- immersion-1-7pointer-events/js/scripts.js
+++ immersion-1-7pointer-events/js/scripts.js
@@ -1,16 +1,28 @@
 (function() {
     var $canvas = document.querySelector('#canvas');
-    
+
+    var isDrawing = false;
+    var previousX = null;
+    var previousY = null;
+
     function handleStart(e) {
-      
+        isDrawing = true;
+
+        // Initiate previ
+        
     }
   
     function handleEnd() {
-      
+      isDrawing = false;
     }
   
     function handleMove(e) {
-      
+      //To prevent drawing on hover
+      if (!isDrawing) {
+          return;
+      }
+
+      // To actually draw...
     }
   
     $canvas.addEventListener("pointerdown", handleStart);
